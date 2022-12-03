09e1fba970dfe81b5cd7c0651984dea7931f9ea6
bstanbri <stanbridgebs@gmail.com>
2022-12-03 17:23:59 +0000

    quadd

---
+++ quadD/main.go
@@ -0,0 +1,63 @@
+package main
+
+// package piscine
+
+import (
+	"fmt"
+
+	"github.com/01-edu/z01"
+)
+
+func QuadA(x, y int) {
+	if x < 0 || y < 0 {
+		fmt.Println("Error Input")
+		return
+	}
+
+	for i := 1; i <= x; i++ {
+		if i == 1 {
+			z01.PrintRune('A')
+		} else if i == x {
+			z01.PrintRune('C')
+		} else {
+			z01.PrintRune('B')
+		}
+	}
+	z01.PrintRune('\n')
+	for j := 1; j <= y-2; j++ {
+		z01.PrintRune('B')
+		for k := 1; k <= x-1; k++ {
+			if k == x-1 {
+				z01.PrintRune('B')
+			} else {
+				z01.PrintRune(' ')
+			}
+		}
+		z01.PrintRune('\n')
+
+	}
+
+	if y > 1 {
+		for i := 1; i <= x; i++ {
+			if i == 1 {
+				z01.PrintRune('A')
+			} else if i == x {
+				z01.PrintRune('C')
+			} else {
+				z01.PrintRune('B')
+			}
+		}
+	}
+
+	z01.PrintRune('\n')
+	z01.PrintRune('\n')
+	z01.PrintRune('\n')
+}
+
+func main() {
+	QuadA(5, 3)
+	QuadA(5, 1)
+	QuadA(1, 1)
+	QuadA(3, 4)
+	QuadA(3, 4)
+}
